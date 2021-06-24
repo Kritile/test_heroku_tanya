ee560f6571e00519c6b653786833f4c05b0b8213
Redhead <stadnikov.pasha@mail.ru>
2021-06-24 16:18:53 +0300

    Update db_config.php

--- config/db_config.php
+++ config/db_config.php
@@ -1,6 +1,6 @@
 <?php 
-    define('DB_USER', "root"); //логин 
-    define('DB_PASSWORD', "tiger"); // пароль 
-    define('DB_DATABASE', "tanya_bd"); // база данных
-    define('DB_SERVER', "mysql");
+    define('DB_USER', "bfe90632ca6dec"); //логин 
+    define('DB_PASSWORD', "54f640bf"); // пароль 
+    define('DB_DATABASE', "heroku_e773a9edf7c3be9"); // база данных
+    define('DB_SERVER', "us-cdbr-east-04.cleardb.com");
 ?>
